921ab9aa871c9ff66024f3befb8409d80123a3ef
James Choi <tienipia@gmail.com>
2024-12-11 22:52:17 +0900

    lang

--- src/extra/index.js
+++ src/extra/index.js
@@ -1,1 +1,93 @@
 export * from './hp_SE2B_CSSLoader';
+
+window.oMessageMap = {
+  'SE_EditingAreaManager.onExit': '내용이 변경되었습니다.',
+  'SE_Color.invalidColorCode':
+    '색상 코드를 올바르게 입력해 주세요. \n\n 예) #000000, #FF0000, #FFFFFF, #ffffff, ffffff',
+  'SE_Hyperlink.invalidURL': '입력하신 URL이 올바르지 않습니다.',
+  'SE_FindReplace.keywordMissing': '찾으실 단어를 입력해 주세요.',
+  'SE_FindReplace.keywordNotFound': '찾으실 단어가 없습니다.',
+  'SE_FindReplace.replaceAllResultP1': '일치하는 내용이 총 ',
+  'SE_FindReplace.replaceAllResultP2': '건 바뀌었습니다.',
+  'SE_FindReplace.notSupportedBrowser':
+    '현재 사용하고 계신 브라우저에서는 사용하실수 없는 기능입니다.\n\n이용에 불편을 드려 죄송합니다.',
+  'SE_FindReplace.replaceKeywordNotFound': '바뀔 단어가 없습니다',
+  'SE_LineHeight.invalidLineHeight': '잘못된 값입니다.',
+  'SE_Footnote.defaultText': '각주내용을 입력해 주세요',
+  'SE.failedToLoadFlash': '플래시가 차단되어 있어 해당 기능을 사용할 수 없습니다.',
+  'SE2M_EditingModeChanger.confirmTextMode':
+    '텍스트 모드로 전환하면 작성된 내용은 유지되나, \n\n글꼴 등의 편집효과와 이미지 등의 첨부내용이 모두 사라지게 됩니다.\n\n전환하시겠습니까?',
+  'SE2M_FontNameWithLayerUI.sSampleText': '가나다라'
+};
+window.oMessageMap_ja_JP = {
+  'SE_EditingAreaManager.onExit': '内容が変更されました。',
+  'SE_Color.invalidColorCode':
+    'カラーコードを正しく入力してください。 \n\n 例) #000000, #FF0000, #FFFFFF, #ffffff, ffffff',
+  'SE_Hyperlink.invalidURL': '入力したURLが正しくありません。',
+  'SE_FindReplace.keywordMissing': 'お探しの単語を入力してください。',
+  'SE_FindReplace.keywordNotFound': 'お探しの単語がありません。',
+  'SE_FindReplace.replaceAllResultP1': '一致する内容が計',
+  'SE_FindReplace.replaceAllResultP2': '件変わりました。',
+  'SE_FindReplace.notSupportedBrowser':
+    '現在ご使用中のブラウザーではご利用いただけない機能です。\n\nご不便をおかけしまして申し訳ございません。',
+  'SE_FindReplace.replaceKeywordNotFound': '変更される単語がありません。',
+  'SE_LineHeight.invalidLineHeight': '誤った値です。',
+  'SE_Footnote.defaultText': '脚注内容を入力してください。',
+  'SE.failedToLoadFlash': 'フラッシュが遮断されているため、この機能は使用できません。',
+  'SE2M_EditingModeChanger.confirmTextMode':
+    'テキストモードに切り換えると、作成された内容は維持されますが、\n\nフォント等の編集効果と画像等の添付内容が消えることになります。\n\n切り換えますか？',
+  'SE2M_FontNameWithLayerUI.sSampleText': 'あいうえお'
+};
+window.oMessageMap_en_US = {
+  'SE_EditingAreaManager.onExit': 'Contents have been changed.',
+  'SE_Color.invalidColorCode': 'Enter the correct color code. \n\n ex) #000000, #FF0000, #FFFFFF, #ffffff, ffffff',
+  'SE_Hyperlink.invalidURL': 'You have entered an incorrect URL.',
+  'SE_FindReplace.keywordMissing': 'Enter the word you wish to find.',
+  'SE_FindReplace.keywordNotFound': 'The word does not exist.',
+  'SE_FindReplace.replaceAllResultP1': 'A total of ',
+  'SE_FindReplace.replaceAllResultP2': ' matching contents have been changed.',
+  'SE_FindReplace.notSupportedBrowser':
+    'Function cannot be used in the browser you are currently using. \n\nSorry for the inconvenience.',
+  'SE_FindReplace.replaceKeywordNotFound': 'No word to change.',
+  'SE_LineHeight.invalidLineHeight': 'Incorrect value.',
+  'SE_Footnote.defaultText': 'Enter footnote details.',
+  'SE.failedToLoadFlash': 'The function cannot be used because flash has been blocked.',
+  'SE2M_EditingModeChanger.confirmTextMode':
+    'The contents remain, but editing effects, including fonts, and attachments, \n\nsuch as images, will disappear when changed to text mode. \n\n Make changes?',
+  'SE2M_FontNameWithLayerUI.sSampleText': 'ABCD'
+};
+window.oMessageMap_zh_CN = {
+  'SE_EditingAreaManager.onExit': '内容有了变化。',
+  'SE_Color.invalidColorCode': '请你输入正确的色相代码。 \n\n 例) #000000, #FF0000, #FFFFFF, #ffffff, ffffff',
+  'SE_Hyperlink.invalidURL': '你输入的URL不符条件。',
+  'SE_FindReplace.keywordMissing': '请你输入要找的词汇。',
+  'SE_FindReplace.keywordNotFound': '没有词汇符合条件。',
+  'SE_FindReplace.replaceAllResultP1': '符合条件的内容改编为',
+  'SE_FindReplace.replaceAllResultP2': '件',
+  'SE_FindReplace.notSupportedBrowser': '这是你现在使用的浏览器不可支持的功能。\n\n麻烦你很道歉。',
+  'SE_FindReplace.replaceKeywordNotFound': '没有词汇要改变。',
+  'SE_LineHeight.invalidLineHeight': '这是有问题的值。',
+  'SE_Footnote.defaultText': '请你输入脚注内容。',
+  'SE.failedToLoadFlash': 'flash被隔绝，不能使用该功能。',
+  'SE2M_EditingModeChanger.confirmTextMode':
+    '转换为text模式就能维持制作内容，\n\n但字体等编辑效果和图像等附件内容都会消失。\n\n你还要继续吗？',
+  'SE2M_FontNameWithLayerUI.sSampleText': 'ABCD'
+};
+
+window.oMessageMap_zh_TW = {
+  'SE_EditingAreaManager.onExit': '內容已被更改。',
+  'SE_Color.invalidColorCode': '請輸入正確的顔色代碼。\n\n例子）#000000, #FF0000, #FFFFFF, #ffffff, ffffff',
+  'SE_Hyperlink.invalidURL': '輸錯URL',
+  'SE_FindReplace.keywordMissing': '請輸入要查詢的單詞。',
+  'SE_FindReplace.keywordNotFound': '要查詢的單詞不存在。',
+  'SE_FindReplace.replaceAllResultP1': '一致的內容總有',
+  'SE_FindReplace.replaceAllResultP2': '件已被更改。',
+  'SE_FindReplace.notSupportedBrowser': '當前的浏覽器上無法使用此功能。\n\n很抱歉給你添麻煩了。',
+  'SE_FindReplace.replaceKeywordNotFound': '要更改的詞彙不存在。',
+  'SE_LineHeight.invalidLineHeight': '爲有錯誤的值。',
+  'SE_Footnote.defaultText': '請輸入注腳內容。',
+  'SE.failedToLoadFlash': 'Flash被屏蔽，導致無法使用此功能。',
+  'SE2M_EditingModeChanger.confirmTextMode':
+    '如轉換爲文本模式，雖然維持已寫成的內容，\n\n但字號等編輯效果和圖像等附加內容都會被消失。\n\n是否轉換？',
+  'SE2M_FontNameWithLayerUI.sSampleText': 'ABCD'
+};
